dc07cd5beb0ed4316da20e1025756bd4024d102a
leemove <923522454@qq.com>
2017-09-06 13:40:39 +0800

    表格demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
-
+import { Hero } from './hero';
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  hero: Hero = new Hero(42, 'SkyDog',
+    'Fetch any object at any distance',
+    'Leslie Rollover');
+  powers = ['Really Smart', 'Super Flexible',
+    'Super Hot', 'Weather Changer'];
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  submitted = false;
+  onSubmit() { this.submitted = true; }
+  get diagnostic() { return JSON.stringify(this.model); }
+  newHero () {
+    this.model = new Hero(42, '', '');
+  }
+}
